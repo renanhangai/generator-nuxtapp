8b586f68d2bf21899a7c5bdbfc036100e23d01e5
renanhangai <hangai.renan@gmail.com>
2018-07-29 23:34:14 -0300

    chore: Added basic generated files

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,1 +1,36 @@
 const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+
+	prompting() {
+		return this.prompt([{
+			name: 'name',
+			type: 'input',
+			message: 'Digite o nome do projeto',
+		}]).then( ( answers ) => {
+			this.anwsers = answers;
+		});
+	}
+
+	writing() {
+		const files = {
+			"gitignore": ".gitignore",
+			"package.json": "package.json",
+			"README.md": "README.md",
+		};
+		for ( const srcFile in files ) {
+			this.fs.copyTpl(  this.templatePath( srcFile ), this.destinationPath( files[srcFile] ), {
+				anwsers: this.anwsers,
+			});
+		}
+	}
+
+	install() {
+		this.installDependencies({
+			npm:   false,
+			bower: false,
+			yarn:  true
+		});
+		this.yarnInstall( [ "nuxt", "cross-env" ], { dev: true } );
+	}
+};
