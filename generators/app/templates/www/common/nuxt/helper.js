fa59ac9779f4fb18cacd30794fa81e16d1eb0b7c
renanhangai <hangai.renan@gmail.com>
2018-08-03 21:32:54 -0300

    chore: Added better helper config generator

--- generators/app/templates/www/common/nuxt/helper.js
+++ generators/app/templates/www/common/nuxt/helper.js
@@ -1,10 +1,47 @@
 const path = require( "path" );
-const BUILD_DIR = path.resolve( __dirname, "../../../dist" );
+const fs = require( "fs" );
+const BUILD_DIR = process.env.APP_BUILD_DIR ? path.resolve( process.env.APP_BUILD_DIR ) : path.resolve( __dirname, "../../../dist" );
+const ROOT_DIR  = path.resolve( __dirname, "../../../" );
 
-module.exports = function( dir ) {
+class NuxtConfigHelper {
+
+	constructor( dir, options ) {
+		this._dir     = dir;
+		this._options = options || {};
+
+		this._baseDir = path.basename( dir );
+	}
+
+	generate() {
+		this.config = {
+			rootDir: ROOT_DIR,
+			srcDir: this._dir,
+			buildDir: path.join( BUILD_DIR, "tmp/.nuxt", this._baseDir ),
+			generate: {
+				dir: path.join( BUILD_DIR, "www", this._baseDir ),
+			},
+			modules: [],
+			plugins: [],
+		};
+		const package = require( path.resolve( ROOT_DIR, 'package.json' ) );
+		const helperFeatures = package.nuxtHelperFeatures || [];
+		helperFeatures.forEach( ( f ) => {
+			const feature = require( path.resolve( __dirname, "./features", f+".js" ) );
+			feature.call( null, this, this._options[f] || {} );
+		});
+
+		this.config.plugins = [].concat( this.config.plugins ).concat( this._options.plugins ).filter( Boolean );
+		this.config.modules = [].concat( this.config.modules ).concat( this._options.modules ).filter( Boolean );
+		this.config.router = this.config.router || {};
+		this.config.router.middleware = [].concat( this.config.router.middleware ).concat( this._options.middleware ).filter( Boolean );
+		
+		return this.config;
+	}
+};
+
+module.exports = function( dir, options ) {
 	const baseDir = path.basename( dir );
-	return {
-		srcDir: dir,
-		buildDir: path.join( BUILD_DIR, "tmp/.nuxt", baseDir ),
-	};
+
+	const helper = new NuxtConfigHelper( dir, options );
+	return helper.generate();
 };
