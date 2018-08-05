2694472f47104000fc167c95232e00e806a1cd07
renanhangai <hangai.renan@gmail.com>
2018-08-05 15:34:40 -0300

    fix: Fixed path for validator resolve

--- generators/features/templates/vuelidate.js
+++ generators/features/templates/vuelidate.js
@@ -3,6 +3,6 @@ const path = require( "path" );
 module.exports = function( config, options ) {
 	config.plugins.push( path.resolve( __dirname, "plugins/vuelidate.js" ) );
 	config.extend.push(function( webpackConfig ) {
-		webpackConfig.resolve.alias["~validators"] = "@/www/common/validators";
+		webpackConfig.resolve.alias["~validators"] = "@@/www/common/validators";
 	});
 }
