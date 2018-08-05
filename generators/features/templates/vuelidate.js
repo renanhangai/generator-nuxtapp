b1f134eb7781c14051d4636e9a6699d85ba4d779
renanhangai <hangai.renan@gmail.com>
2018-08-05 15:09:57 -0300

    chore: Added alias for vuelidate validators

--- generators/features/templates/vuelidate.js
+++ generators/features/templates/vuelidate.js
@@ -2,4 +2,7 @@ const path = require( "path" );
 
 module.exports = function( config, options ) {
 	config.plugins.push( path.resolve( __dirname, "plugins/vuelidate.js" ) );
+	config.extend.push(function( webpackConfig ) {
+		webpackConfig.resolve.alias["~validators"] = "@/www/common/validators";
+	});
 }
