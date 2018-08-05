434440001935f0b3cdb4ec67bf59c2cb7b00dc19
renanhangai <hangai.renan@gmail.com>
2018-08-05 17:36:42 -0300

    chore: Apollo now defaults to config file if string is passed

--- generators/features/templates/apollo.js
+++ generators/features/templates/apollo.js
@@ -1,5 +1,12 @@
 module.exports = function( config, options ) {
 	if ( !options )
 		return;
+	if ( typeof(options) === 'string' ) {
+		options = {
+			clientConfigs: {
+				default: options,
+			},
+		};
+	}
 	config.modules.push( [ "@nuxtjs/apollo", options ] );
 }
